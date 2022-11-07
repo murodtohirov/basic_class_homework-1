8b5a2a65f1bb090daa5238bf4c3477c9b5aa421d
Пользователь <murodjontohirov2701@gmail.com>
2022-11-07 13:39:35 +0500

    class06

--- bin/basic_class_homework06.dart
+++ bin/basic_class_homework06.dart
@@ -4,9 +4,15 @@
 Create a "Person" class that has a name("name") and a age("age")
 */
 
+import 'dart:web_audio';
+
 class Person {
   String name = "name";
   String age = "age";
 }
 
-void main() {}
+void main() {
+  Person x = Person();
+  print(x.age);
+  print(x.name);
+}
